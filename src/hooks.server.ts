c10b167b77d8873078115593332bdd8f513d1161
EugeneDraitsev <eugene.draitsev@yubico.com>
2025-04-18 00:22:32 +0200

    Add debug logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,5 +2,6 @@ import { auth } from '$lib/auth'; // path to your auth file
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function handle({ event, resolve }) {
+  console.log('event.request.url', event.request.url);
   return svelteKitHandler({ event, resolve, auth });
 }
