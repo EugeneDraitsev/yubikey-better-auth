3d6c251195bea80f620cdb3932cb8e2de1ccd2da
EugeneDraitsev <eugene.draitsev@yubico.com>
2025-04-18 00:16:06 +0200

    Fix 404

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,5 @@ import { auth } from '$lib/auth'; // path to your auth file
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function handle({ event, resolve }) {
-  if (event.url.pathname.startsWith('/api/')) {
-    return svelteKitHandler({ event, resolve, auth });
-  }
-  return resolve(event);
+  return svelteKitHandler({ event, resolve, auth });
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@
 import { betterAuth } from 'better-auth';
 import { anonymous } from 'better-auth/plugins';
 import { passkey } from 'better-auth/plugins/passkey';
-import { createAuthMiddleware } from 'better-auth/api';
 import { createClient } from '@libsql/client';
 import { LibsqlDialect } from '@libsql/kysely-libsql';
 import { env } from '$env/dynamic/private';
@@ -19,24 +18,8 @@ export const auth = betterAuth({
   secret: env.BETTER_AUTH_SECRET,
   trustedOrigins: [env.BETTER_BASE_URL],
   database: db as any,
-  hooks: {
-    after: createAuthMiddleware(async (ctx) => {
-      if (ctx.path === '/passkey/generate-register-options') {
-        const result = ctx.context.returned as {
-          user: { id: string; displayName: string; name?: string };
-        };
-
-        const modified = {
-          ...result
-          // user: {
-          //   ...result?.user,
-          //   displayName: result?.user?.displayName || result?.user?.name
-          // }
-        };
-
-        return ctx.json(modified);
-      }
-    })
+  advanced: {
+    useSecureCookies: true
   },
   plugins: [
     anonymous({
