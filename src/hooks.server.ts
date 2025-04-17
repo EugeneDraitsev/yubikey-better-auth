c62192026f867ef4b0543cc778bf4b42b45b0b4e
EugeneDraitsev <eugene.draitsev@yubico.com>
2025-04-18 01:42:15 +0200

    Merge to latest main

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,6 @@
 import { auth } from '$lib/auth'; // path to your auth file
 import { svelteKitHandler } from 'better-auth/svelte-kit';
-import { env } from '$env/dynamic/private';
 
 export async function handle({ event, resolve }) {
-  console.log('event.request.url', event.request.url);
-  console.log('BETTER_BASE_URL', env.BETTER_BASE_URL);
   return svelteKitHandler({ event, resolve, auth });
 }
